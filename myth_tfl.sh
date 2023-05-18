572532b0c33d5659ac3c082b88e0cec77af6d7f9
Mylo Cyrus <irahiggins740@gmail.com>
2023-05-18 14:53:31 -0700

    Add files via upload

---
+++ myth_tfl.sh
@@ -0,0 +1,47 @@
+<files>
+<file name="Myth - The Fallen Lords (1997)(Eidos).cue" source="original">
+<format>Cue Sheet</format>
+<mtime>1386408477</mtime>
+<size>126</size>
+<md5>a4b7bb3119bc11ca358cdd127f09ad59</md5>
+<crc32>28f42615</crc32>
+<sha1>ec8d4bb06747f342bc55a3e10f7328fcf809df80</sha1>
+</file>
+<file name="Myth - The Fallen Lords (1997)(Eidos).iso" source="original">
+<format>ISO Image</format>
+<mtime>1386408509</mtime>
+<size>551695456</size>
+<md5>a27f5ed953f2fadb8e2a29e99168b82a</md5>
+<crc32>ecde875b</crc32>
+<sha1>9ddbcf41587cfc0c3b3801df886607dfab22976b</sha1>
+</file>
+<file name="Myth_-_The_Fallen_Lords_1997_Eidos_archive.torrent" source="metadata">
+<btih>f360af80ee1001543985c718c4cf543d5e9fa11e</btih>
+<mtime>1515570784</mtime>
+<size>23079</size>
+<md5>584e4b0669d1bf272318153475c323ee</md5>
+<crc32>dc4fa751</crc32>
+<sha1>aba3a3f9725e0e085b7875aadcb046e35f518061</sha1>
+<format>Archive BitTorrent</format>
+</file>
+<file name="Myth_-_The_Fallen_Lords_1997_Eidos_files.xml" source="original">
+<format>Metadata</format>
+<md5>580e2c67074d2be99de1bbfa09cedeb6</md5>
+</file>
+<file name="Myth_-_The_Fallen_Lords_1997_Eidos_meta.sqlite" source="original">
+<format>Metadata</format>
+<mtime>1386408525</mtime>
+<size>10240</size>
+<md5>c2039d7a86a1aab471560822ff6939c4</md5>
+<crc32>74d39a3b</crc32>
+<sha1>ed826620d8b506373eba8b453bd0a5c423298424</sha1>
+</file>
+<file name="Myth_-_The_Fallen_Lords_1997_Eidos_meta.xml" source="original">
+<format>Metadata</format>
+<mtime>1559310033</mtime>
+<size>615</size>
+<md5>b46c8c39be7ead982aaec71827eec498</md5>
+<crc32>32698c0c</crc32>
+<sha1>8960660fccda39ad4910c3131b9017d3818564b5</sha1>
+</file>
+</files>
