ac422d3dcb57985791b927ecf43f59bfc8011505
Mylo Cyrus <irahiggins740@gmail.com>
2023-05-19 12:26:33 -0700

    Add files via upload

---
+++ myloz_server.cpp
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" class="__meteor-css__" href="/e599de1fb99b4b038bea3430966f7a8a26372f83.css?meteor_css_resource=true">
+<meta charset="UTF-8"><meta name="fragment" content="!"><meta name="format-detection" content="telephone=no, date=no, address=no, email=no"><meta name="viewport" content="width=device-width, initial-scale=1"><script type="text/javascript" src="https://analytics.ostr.io/wdyCpoj4gif25xSRC.js"></script><script async="" src="https://www.googletagmanager.com/gtag/js?id=UA-146797393-1"></script><script type="text/javascript">window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-146797393-1', {send_page_view: false});
+</script><meta content="website" name="og:type" property="og:type"><meta content="summary" name="twitter:card"><meta name="apple-mobile-web-app-status-bar-style" content="default"><meta name="apple-mobile-web-app-capable" content="yes"><meta name="mobile-web-app-capable" content="yes"><meta name="og:site_name" property="og:site_name" content="MD5Hashing"><link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"><link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"><link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"><link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png"><link rel="icon" type="image/png" sizes="194x194" href="/favicon-194x194.png"><link rel="manifest" href="/manifest.webmanifest"><link rel="mask-icon" href="/safari-pinned-tab.svg" color="#bd362f"><meta name="apple-mobile-web-app-title" content="md5#"><meta name="application-name" content="md5#"><meta name="msapplication-TileColor" content="#bd362f"><meta name="msapplication-TileImage" content="/mstile-144x144.png"><meta name="theme-color" content="#bd362f"><meta name="fb:app_id" property="fb:app_id" content="1794741257419580"><title>Hash, hashing, and encryption toolkit</title>
+
+</head>
+<body>
+
+
+
+  <script type="text/javascript">__meteor_runtime_config__ = JSON.parse(decodeURIComponent("%7B%22meteorRelease%22%3A%22METEOR%401.5.4.2%22%2C%22meteorEnv%22%3A%7B%22NODE_ENV%22%3A%22production%22%2C%22TEST_METADATA%22%3A%22%7B%7D%22%7D%2C%22PUBLIC_SETTINGS%22%3A%7B%22stage%22%3A%22production%22%2C%22secure%22%3Atrue%2C%22ga%22%3A%7B%22id%22%3A%22UA-146797393-1%22%7D%7D%2C%22ROOT_URL%22%3A%22https%3A%2F%2Fmd5hashing.net%22%2C%22ROOT_URL_PATH_PREFIX%22%3A%22%22%2C%22DDP_DEFAULT_CONNECTION_URL%22%3A%22https%3A%2F%2Fmd5hashing.net%22%2C%22appId%22%3A%221yyb8dxdjzh9kehs7f7%22%2C%22autoupdateVersion%22%3A%22979215eaadf8ae5bc99b1a5cc390ab733c6439bf%22%2C%22autoupdateVersionRefreshable%22%3A%22b3affe8fd7a6f265565e07c647e7286e42d09ea2%22%2C%22autoupdateVersionCordova%22%3A%22none%22%7D"))</script>
+
+  <script type="text/javascript" src="/f670ef9b0b76f130b0a298b9b37f3b5fd8393c39.js?meteor_js_resource=true"></script>
+
+
+</body>
+</html>
+}{
+
+web proxy server custom header:
+
+plain:   name: 'x-ostrio-domain', value: 'BoE1ev1iuzW08Wc3Lu9B8huVP5BhpfLj5YMYjCWG_dW'
+PHP:     header("x-ostrio-domain: BoE1ev1iuzW08Wc3Lu9B8huVP5BhpfLj5YMYjCWG_dW");
+Apache:  Header set x-ostrio-domain "BoE1ev1iuzW08Wc3Lu9B8huVP5BhpfLj5YMYjCWG_dW"
+nginx:   add_header x-ostrio-domain BoE1ev1iuzW08Wc3Lu9B8huVP5BhpfLj5YMYjCWG_dW;
+HAProxy: http-response add-header x-ostrio-domain BoE1ev1iuzW08Wc3Lu9B8huVP5BhpfLj5YMYjCWG_dW
+Varnish: set resp.http.x-ostrio-domain = BoE1ev1iuzW08Wc3Lu9B8huVP5BhpfLj5YMYjCWG_dW;
+
+$ server: meta tags = "
+
+<meta name="ostrio-domain" content="BoE1ev1iuzW08Wc3Lu9B8huVP5BhpfLj5YMYjCWG_dW">
+
+meta(name="ostrio-domain" content="BoE1ev1iuzW08Wc3Lu9B8huVP5BhpfLj5YMYjCWG_dW")
+
+"
+
+$ server: DNS = "
+
+ostrio-domain: BoE1ev1iuzW08Wc3Lu9B8huVP5BhpfLj5YMYjCWG_dW
+
+"
+
+$ server: download = "
+
+ostrio-domain: BoE1ev1iuzW08Wc3Lu9B8huVP5BhpfLj5YMYjCWG_dW
+
+"
+
+
+
+}}
+
+
+017519248AABA32BC443BE7249941801C93D0DBD
+magnet:?xt=urn:btih:b3ba4523849287f76bbf6f123cadd2edd33182c5&xt=urn:btmh:1220c3cf73149c5e6ed49a6d26c77a60099b4f26dd55757fc3088c497c940bbe42f6&dn=bytecoin-desktop-3.5.1-win64.msi\
+magnet:?xt=urn:btih:c593b996df1180bbede122a4c40c007e97fa0d59&xt=urn:btmh:1220990e403f71df83c2c6540427262e9fd4785f06fc4eb0230dae8c9d05f30a8710&dn=xmrig.exe
